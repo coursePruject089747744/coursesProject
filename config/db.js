ebd228332b775e69dd1e15eee092743f1358a350
yoni3593 <yoni263593@gmail.com>
2024-12-05 11:09:52 +0200

    change CONNECTION_STRING

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI); // חיבור ללא פרמטרים נוספים
+    await mongoose.connect(process.env.CONNECTION_STRING ); // חיבור ללא פרמטרים נוספים
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
